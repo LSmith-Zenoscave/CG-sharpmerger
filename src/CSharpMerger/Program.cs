1dd35f709355dce16e485bf1f0c754bad96cd923
LSmith-Zenoscave <lsmith@zenoscave.com>
2020-10-31 12:39:14 -0600

    Add CommandLine Parser and refactor to cli args
    Signed-off-by: LSmith-Zenoscave <lsmith@zenoscave.com>

--- src/CSharpMerger/Program.cs
+++ src/CSharpMerger/Program.cs
@@ -3,46 +3,74 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandLine;
+
 
 namespace CodeCopier
 {
     class Program
     {
-        private static string m_config = "config.data";
-        private static string m_path = @"",
-            m_outputPath = @"";
+        class Options
+        {
+            [Option('p', "path", Required = true, HelpText = "Base directory path to collect together.")]
+            public string BasePath { get; set; }
+            
+            [Option('o', "output", Required = true, HelpText = "Destination file path for collected source file.")]
+            public string OutputPath { get; set; }
 
-        static async Task Main()
+            [Option('w', "watch", Default = false, HelpText = "Enable path watcher.")]
+            public bool Watch { get; set; }
+        }
+
+        private static Options options;
+
+        static async Task Main(string[] args)
         {
             var lastUpdated = DateTime.MinValue;
 
-            Console.WriteLine("Write R to reuse last or directory path for new session");
-            var command = Console.ReadLine();
-            if (command.ToLower() == "r")
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(options =>
             {
-                var contents = File.ReadAllLines(m_config);
-                m_path = contents[0];
-                m_outputPath = contents[1];
-            }
-            else
+                Program.options = options;
+            });
+        
+            if (options == null)
             {
-                m_path = command;
-                Console.WriteLine("Write target destination file");
-                m_outputPath = Console.ReadLine();
-                File.WriteAllLines(m_config, new string[] { m_path, m_outputPath });
+                return; 
             }
 
-
-            while (true)
+            do
             {
                 try
                 {
                     await Task.Delay(500);
-                    var files = GetFiles().Where(f => f != m_outputPath).ToArray();
+                    var files = GetFiles().Where(f => f != options.OutputPath).ToArray();
                     var lastEdited = FindLastEdited(files);
                     if (lastEdited == lastUpdated) continue;
-                    var mergedFile = CreateMergedCsFile(files.Select(f => File.ReadAllLines(f)).SelectMany(f => f).ToList(), lastEdited);
-                    File.WriteAllText(m_outputPath, mergedFile);
+                    var filelines = files.Select(f => File.ReadAllLines(f));
+
+                    var contentsets = new Dictionary<string, List<string>>();
+                    foreach (var file in filelines)
+                    {
+                        var ns = file.First(l => l.StartsWith("namespace")).Split()[1];
+                        var lines = file.Where(l =>
+                        {
+                            return (
+                                !l.StartsWith("namespace") &&
+                                !l.StartsWith("{") &&
+                                !l.StartsWith("}")
+                            );
+                        }).ToList();
+
+                        if (!contentsets.ContainsKey(ns))
+                        {
+                            contentsets[ns] = new List<string>();
+                        }
+                        contentsets[ns].AddRange(lines);
+                    }
+
+
+                    var mergedFile = CreateMergedCsFile(contentsets, lastEdited);
+                    File.WriteAllText(options.OutputPath, mergedFile);
                     lastUpdated = lastEdited;
                     Console.WriteLine("Updated: " + lastEdited.ToString("MM/dd/yyyy H:mm \n"));
                 }
@@ -51,19 +79,32 @@ namespace CodeCopier
                     Console.Error.WriteLine(e.Message);
                 }
             }
+            while (options.Watch);
         }
 
-        private static string CreateMergedCsFile(List<string> content, DateTime edited)
+        private static string CreateMergedCsFile(Dictionary<string, List<string>> content, DateTime edited)
         {
-            var usings = content.Where(c => c.StartsWith("using", StringComparison.Ordinal)).Distinct().ToList();
-            content.RemoveAll(c => c.StartsWith("using", StringComparison.Ordinal));
+            var usings = new List<string>();
+            foreach (var contentset in content)
+            {
+                usings.AddRange(contentset.Value.Where(c => c.StartsWith("using", StringComparison.Ordinal)).Distinct());
+                contentset.Value.RemoveAll(c => c.StartsWith("using", StringComparison.Ordinal));
+            }
 
-            return string.Join("\n", usings) + "\n\n\n // LastEdited: " + edited.ToString("dd/MM/yyyy H:mm \n\n\n") + string.Join("\n", content);
+            var namespaces = content.Select(n =>
+            {
+                var lines = new List<string>() { $"namespace {n.Key}", "{" };
+                lines.AddRange(n.Value);
+                lines.Add("}");
+                return String.Join("\n", lines);
+            });
+
+            return string.Join("\n", usings) + "\n\n\n// LastEdited: " + edited.ToString("dd/MM/yyyy H:mm \n\n\n") + string.Join("\n", namespaces);
         }
 
         private static string[] GetFiles()
         {
-            return Directory.GetFiles(m_path, "*.cs", SearchOption.AllDirectories)
+            return Directory.GetFiles(options.BasePath, "*.cs", SearchOption.AllDirectories)
                 .Where(f => !f.ToLower().Contains("/bin/") && !f.ToLower().Contains("/obj/") && Path.GetFileName(f) != "AssemblyInfo.cs")
                 .ToArray();
         }
